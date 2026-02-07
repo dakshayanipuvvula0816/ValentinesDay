7721b56bf1baf62cc5d86bce6cef519cf4db0ed0
dakshayanipuvvula0816 <dakshayani.puvvula.0816@gmail.com>
2026-02-07 10:10:31 +0530

    Create game7.js

---
+++ js/game7.js
@@ -0,0 +1,68 @@
+const board = document.getElementById("gameBoard");
+
+const images = [
+  "1.jpg","2.jpg","3.jpg","4.jpg","5.jpg",
+  "6.jpg","7.jpg","8.jpg","9.jpg","10.jpg"
+];
+
+// Duplicate & shuffle
+let cards = [...images, ...images];
+cards.sort(() => Math.random() - 0.5);
+
+let firstCard = null;
+let secondCard = null;
+let lockBoard = false;
+let matchedPairs = 0;
+
+cards.forEach(img => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  card.innerHTML = `
+    <div class="card-face front">❤️</div>
+    <div class="card-face back">
+      <img src="../images/${img}">
+    </div>
+  `;
+
+  card.addEventListener("click", () => {
+    if (lockBoard || card === firstCard) return;
+
+    card.classList.add("flip");
+
+    if (!firstCard) {
+      firstCard = card;
+      return;
+    }
+
+    secondCard = card;
+    lockBoard = true;
+
+    const img1 = firstCard.querySelector("img").src;
+    const img2 = secondCard.querySelector("img").src;
+
+    if (img1 === img2) {
+      matchedPairs++;
+      resetTurn();
+
+      if (matchedPairs === images.length) {
+        setTimeout(() => {
+          alert("You matched all our hearts 💖");
+        }, 400);
+      }
+    } else {
+      setTimeout(() => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+        resetTurn();
+      }, 1000);
+    }
+  });
+
+  board.appendChild(card);
+});
+
+function resetTurn() {
+  [firstCard, secondCard] = [null, null];
+  lockBoard = false;
+}
