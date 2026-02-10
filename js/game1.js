18c0369c6d3c769b5a21aaf5b940aa0e99966427
dakshayanipuvvula0816 <dakshayani.puvvula.0816@gmail.com>
2026-02-10 23:07:40 +0530

    Update game1.js

--- js/game1.js
+++ js/game1.js
@@ -80,7 +80,7 @@ puzzles.forEach(puzzle => {
     if (solved && !board.classList.contains("done")) {
       board.classList.add("done");
       solvedCount++;
-      if (solvedCount === puzzles.length){
+      if (solvedCount === 3) fireworks();
         fireworks();
         showWinPopup();
     }
